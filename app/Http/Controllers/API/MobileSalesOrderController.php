238d513bf616511cb09f23ac4ced1608c73553fc
Junatan  Marc Cadayona <ljohnolayvar32@gmail.com>
2025-10-18 01:43:04 +0800

    remove encode

--- app/Http/Controllers/API/MobileSalesOrderController.php
+++ app/Http/Controllers/API/MobileSalesOrderController.php
@@ -123,7 +123,7 @@ class MobileSalesOrderController extends Controller
     public function analytics(AnalyticsServices $analytics)
     {
         return response()->json([
-            'analytics' => json_encode($analytics->medRepDashboard())
+            'analytics' => $analytics->medRepDashboard()
         ]);
     }
 }
